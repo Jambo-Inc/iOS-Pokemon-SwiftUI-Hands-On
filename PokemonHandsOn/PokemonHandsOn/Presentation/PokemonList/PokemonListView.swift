ea734335a74b915c747b8ae8e0afe306a5630b06
MichitakaIshida <ishida.jambo@gmail.com>
2024-03-20 19:03:10 +0900

    手順1 画面にモンスターボールを1つ表示してみる

--- PokemonHandsOn/PokemonHandsOn/Presentation/PokemonList/PokemonListView.swift
+++ PokemonHandsOn/PokemonHandsOn/Presentation/PokemonList/PokemonListView.swift
@@ -12,6 +12,29 @@ struct PokemonListView: View {
     var body: some View {
 
         Text("手順1、モンスターボールを表示する")
+            .frame(width: 100, height: 100)
+            // 背景にコンテンツを設置するモディファイア
+            .background() {
+                // {}内の要素を前後に表示する
+                ZStack {
+                        // 赤色の円を表示
+                    Circle()
+                        .fill(.red)
+                        .frame(height: 100)
+                        // 白色の長方形を表示
+                    Rectangle()
+                        .fill(.white)
+                        .frame(width: 100)
+                        // 下に50ずらした部分から描画を始める処理
+                        .offset(y: 50)
+                }
+            }
+            // テキストのframeを円に描く処理
+            // 枠線を黒色に、ラインの太さを1に指定
+            .overlay {
+                Circle()
+                    .stroke(.black, lineWidth: 1)
+            }
     }
 }
 
